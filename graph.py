7deeb993a6f57c24774ee86e3aaa62297672e7a5
edlor <56137280+edlor@users.noreply.github.com>
2021-12-10 20:00:24 +0100

    Added 'merge_graph' function

--- graph.py
+++ graph.py
@@ -53,5 +53,56 @@ class Graph:
         
         return
 
+
+def merge_dicts(a, b, subkey):
+    
+    if not a:
+        return b
+    
+    if not b:
+        return a
+    
+    c = a
         
+    for k in b.keys():
+        try:
+            if b[k][subkey] < c[k][subkey]:
+                c[k] = {subkey: b[k][subkey]}
+        except KeyError:
+            c[k] = {subkey: b[k][subkey]}
+
+    return c
+    
+
+def merge_graphs(G, J, assign_weights = True):
+    
+    new_graph = Graph()
+    
+    # all unique nodes in both graphs
+    all_nodes = set(list(G.nodes.keys()) + list(J.nodes.keys()))
+    
+    for node in all_nodes:
+        
+        # find all edges in both graphs for that node
+        try:
+            G_node_edges = G[node]
+        except KeyError:
+            G_node_edges = {}
+            
+        try:
+            J_node_edges = J[node]
+        except KeyError:
+            J_node_edges = {}
+            
+        # merge them so we get a unique dictionary
+        # for edges in common only that with the earlier timestamp
+        # is kept
+        node_edges = merge_dicts(G_node_edges, J_node_edges, "time")
+           
+        # add those edges to the new graph
+        for edge in node_edges:
+            new_graph.add_edge(node, edge, 
+                               {"time": node_edges[edge]["time"]})
+    
+    return new_graph    
         
